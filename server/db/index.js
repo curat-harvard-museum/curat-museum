f94d9c40685eb4bc9b7e8e6da77455e8e0243db4
Jasmine Cheung <evilgummii@gmail.com>
2022-10-07 16:37:15 -0400

    added models

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,15 @@
-//this is the access point for all things database related!
+const db = require("./db");
 
-const db = require('./db')
+const User = require("./models/User");
+const Comment = require("./models/Comment");
 
-const User = require('./models/User')
-
-//associations could go here!
+User.belongsToMany(Comment, { through: "userComment" });
+Comment.belongsToMany(User, { through: "userComment" });
 
 module.exports = {
   db,
   models: {
     User,
+    Comment,
   },
-}
+};
