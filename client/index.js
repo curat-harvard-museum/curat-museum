f7635210acf68e0b1c79b56ec4b0f3c40ebbca85
Jordy <jordynavalee@gmail.com>
2022-10-10 14:46:08 -0500

    adds queryclientprovider

--- client/index.js
+++ client/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import store from "./store";
@@ -7,14 +8,18 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
 
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="*" element={<App />} />
-        </Routes>
-      </Router>
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path="*" element={<App />} />
+          </Routes>
+        </Router>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
