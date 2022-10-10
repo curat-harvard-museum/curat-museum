16d6ec39e88c79473ea2a98596aaab27ffe072a3
Joyce-Wong <joycekwo12@gmail.com>
2022-10-10 14:50:30 -0400

    database seeds but some descriptions are null

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+const path = require("path");
+const webpack = require('webpack');
+const dotenv = require('dotenv').config( {
+  path: path.join(__dirname, '.env')
+} );
+
 module.exports = {
   entry: [
     './client/index.js'
@@ -7,6 +13,11 @@ module.exports = {
     filename: './public/bundle.js'
   },
   devtool: 'source-map',
+  plugins: [
+    new webpack.DefinePlugin( {
+      "process.env": dotenv.parsed
+    } ),
+  ],
   module: {
     rules: [
       {
