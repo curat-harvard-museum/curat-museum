97c4bda621bfdb10044c69139c152413d32135db
Joyce-Wong <joycekwo12@gmail.com>
2022-10-13 15:36:29 -0400

    cleaned up profile

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,22 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import { useSelector } from "react-redux";
-import {useState} from "react";
-import {Link} from "react-router-dom";
+
 
 /**
  * COMPONENT
  */
 const Profile = (props) => {
   const username = useSelector((state) => state.auth.username);
-  // console.log("state.auth.objects", state.auth.objects)
-  // const [favorites, setFavorites] = useState(state.auth.objects)
   const favorites = useSelector((state) => state.auth.objects);
-  console.log("favorites", favorites)
 
   return (
     <>
       <h3>Welcome, {username}</h3>
-      {/* <h3>{favorite.title}</h3> */}
       <div>
         {favorites?.map((favorite) => 
           ( <>
@@ -27,19 +22,6 @@ const Profile = (props) => {
           </>
           ))}
       </div>
-      {/* <h3>{favorites}</h3> */}
-    {/* <div className="profile-container">
-      <div>{favorites.map((favorite) => (
-        <div key={favorite.id}>
-          <Link to={`/object/${favorite.id}`} >{favorite.title}
-          </Link>
-          {favorite.primaryimageurl ? (<img src={favorite.primaryimageurl}
-          alt="{favorite.title} by {favorite.people[0].name}"/>) : null}
-          <p>Artist: {favorite.people[0].name}</p>
-          <p>Description: {favorite.description}</p>
-          </div>
-      ))}</div>
-    </div> */}
     </>
   );
 };
