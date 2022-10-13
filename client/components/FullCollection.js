56ba323b59f36d670fe066e3a713eaea784ca951
Jordy <jordynavalee@gmail.com>
2022-10-13 08:33:07 -0500

    resolves data access on Full Collection

--- client/components/FullCollection.js
+++ client/components/FullCollection.js
@@ -1,54 +1,168 @@
-import React from "react";
+import React, { useEffect, useRef, useCallback } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { useInfiniteQuery } from "react-query";
 import apiClient from "../../http-common";
 import { Link } from "react-router-dom";
 
 function AllObjects() {
-  const { data, refetch: getAllObjects } = useQuery(
-    ["query-objects"],
+  const observerElem = useRef(null);
 
-    async () => {
-      return await apiClient.get(
-        `/object?apikey=a58b1ca8-7853-40e4-8734-f634a87b9be7&page=83&size=100`
-      );
-    }
+  const fetchPage = async (page) => {
+    const response = await fetch(
+      `https://api.harvardartmuseums.org/object?apikey=a58b1ca8-7853-40e4-8734-f634a87b9be7&page=${page}&size=10`
+    );
+    return await response.json();
+  };
+
+  const { data, isSuccess, hasNextPage, fetchNextPage, isFetchingNextPage } =
+    useInfiniteQuery(["objects"], ({ pageParam = 1 }) => fetchPage(pageParam), {
+      getNextPageParam: (lastPage, allPages) => lastPage.nextCursor,
+      getPreviousPageParam: (firstPage, allPages) => firstPage.prevCursor,
+
+      // getNextPageParam: (lastPage, allPages) => {
+      //   const nextPage = allPages.length + 1;
+      //   return lastPage.items.length !== 0 ? nextPage : undefined;
+      // },
+    });
+
+  useEffect(() => {
+    let fetching = false;
+    const handleScroll = async (e) => {
+      const { scrollHeight, scrollTop, clientHeight } =
+        e.target.scrollingElement;
+      if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.2) {
+        fetching = true;
+        if (hasNextPage) await fetchNextPage();
+        fetching = false;
+      }
+    };
+    document.addEventListener("scroll", handleScroll);
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, [fetchNextPage, hasNextPage]);
+
+  const handleObserver = useCallback(
+    (records) => {
+      const [target] = records;
+      if (target.isIntersecting && hasNextPage) {
+        fetchNextPage();
+      }
+    },
+    [fetchNextPage, hasNextPage]
   );
 
-  function handleGetObjects() {
-    getAllObjects();
-  }
+  useEffect(() => {
+    const element = observerElem.current;
+    const option = { threshold: 0 };
 
-  console.log(data?.data.info.next);
+    const observer = new IntersectionObserver(handleObserver, option);
+    observer.observe(element);
+    return () => observer.unobserve(element);
+  }, [fetchNextPage, hasNextPage, handleObserver]);
+
+  // console.log("data", data?.pages[0].records);
 
   return (
     <>
       <div className="grid-container">
-        {data?.data.records
-          .filter((record) => record.primaryimageurl)
-          .map((record) => (
-            <Link key={record.id} to={`/object/${record.id}`}>
-              <>
-                <img
-                  className="single-grid-image"
-                  src={record.primaryimageurl}
-                  alt="{record.title} by {record.people[0].name} "
-                ></img>
-                <div className="caption-text title-caption-text">
-                  {record.title}
-                </div>
-                <div className="caption-text person-caption-text">
-                  {/* {record.people[0].name ? record.people[0].name : null} */}
-                </div>
-                <div className="caption-text classification-caption-text">
-                  {record.classification}
-                </div>
-              </>
-            </Link>
-          ))}
+        {isSuccess &&
+          data?.pages[0].records
+            .filter((record) => record.primaryimageurl)
+            .map((record) => (
+              <Link key={record.id} to={`/object/${record.id}`}>
+                <>
+                  <img
+                    className="single-grid-image"
+                    src={record.primaryimageurl}
+                    alt="{record.title} by {record.people[0].name} "
+                  ></img>
+                  <div className="caption-text title-caption-text">
+                    {record.title}
+                  </div>
+                  <div className="caption-text person-caption-text">
+                    {/* {record.people[0].name ? record.people[0].name : null} */}
+                  </div>
+                  <div className="caption-text classification-caption-text">
+                    {record.classification}
+                  </div>
+                </>
+              </Link>
+            ))}
+
+        <div ref={observerElem}>
+          {isFetchingNextPage && hasNextPage ? "loading..." : "fin."}
+        </div>
       </div>
     </>
   );
 }
 
 export default AllObjects;
+
+// function AllObjects() {
+//   const { data, refetch: getAllObjects } = useQuery(
+//     ["query-objects"],
+
+//     async () => {
+//       return await apiClient.get(
+//         `/object?apikey=a58b1ca8-7853-40e4-8734-f634a87b9be7&page=83&size=100`
+//       );
+//     }
+//   );
+
+//   function handleGetObjects() {
+//     getAllObjects();
+//   }
+
+//   console.log(data?.data.info.next);
+// }
+
+// function AllObjects() {
+//   const { data, refetch: getAllObjects } = useQuery(
+//     ["query-objects"],
+
+//     async () => {
+//       return await apiClient.get(
+//         `/object?apikey=a58b1ca8-7853-40e4-8734-f634a87b9be7&page=83&size=100`
+//       );
+//     }
+//   );
+
+//   function handleGetObjects() {
+//     getAllObjects();
+//   }
+
+//   console.log(data?.data.info.next);
+
+//   return (
+//     <>
+//       <div className="grid-container">
+//         {data?.data.records
+//           .filter((record) => record.primaryimageurl)
+//           .map((record) => (
+//             <Link key={record.id} to={`/object/${record.id}`}>
+//               <>
+//                 <img
+//                   className="single-grid-image"
+//                   src={record.primaryimageurl}
+//                   alt="{record.title} by {record.people[0].name} "
+//                 ></img>
+//                 <div className="caption-text title-caption-text">
+//                   {record.title}
+//                 </div>
+//                 <div className="caption-text person-caption-text">
+//                   {/* {record.people[0].name ? record.people[0].name : null} */}
+//                 </div>
+//                 <div className="caption-text classification-caption-text">
+//                   {record.classification}
+//                 </div>
+//               </>
+//             </Link>
+//           ))}
+//       </div>
+//     </>
+//   );
+// }
+
+// export default AllObjects;
--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import store from "./store";
@@ -13,8 +13,8 @@ const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
         <Provider store={store}>
           <Router>
             <Routes>
@@ -22,7 +22,7 @@ root.render(
             </Routes>
           </Router>
         </Provider>
-      </QueryClientProvider>
-    </ChakraProvider>
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
