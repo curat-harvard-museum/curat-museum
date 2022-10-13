ba9fad2344542cd2f056b5e51205a7b29d42bd39
Joyce-Wong <joycekwo12@gmail.com>
2022-10-13 15:32:16 -0400

    favorites render on a person's profile when they log in

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,16 +1,46 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import {useState} from "react";
+import {Link} from "react-router-dom";
 
 /**
  * COMPONENT
  */
 const Profile = (props) => {
   const username = useSelector((state) => state.auth.username);
+  // console.log("state.auth.objects", state.auth.objects)
+  // const [favorites, setFavorites] = useState(state.auth.objects)
+  const favorites = useSelector((state) => state.auth.objects);
+  console.log("favorites", favorites)
 
   return (
-    <div>
+    <>
       <h3>Welcome, {username}</h3>
-    </div>
+      {/* <h3>{favorite.title}</h3> */}
+      <div>
+        {favorites?.map((favorite) => 
+          ( <>
+          <div>Title: {favorite.title}</div>
+          <div>Artist: {favorite.artist}</div>
+          <img src={favorite.primaryimageurl}></img>
+          <div>Description: {favorite.description}</div>
+          </>
+          ))}
+      </div>
+      {/* <h3>{favorites}</h3> */}
+    {/* <div className="profile-container">
+      <div>{favorites.map((favorite) => (
+        <div key={favorite.id}>
+          <Link to={`/object/${favorite.id}`} >{favorite.title}
+          </Link>
+          {favorite.primaryimageurl ? (<img src={favorite.primaryimageurl}
+          alt="{favorite.title} by {favorite.people[0].name}"/>) : null}
+          <p>Artist: {favorite.people[0].name}</p>
+          <p>Description: {favorite.description}</p>
+          </div>
+      ))}</div>
+    </div> */}
+    </>
   );
 };
 
--- server/db/models/User.js
+++ server/db/models/User.js
@@ -58,7 +58,7 @@ User.findByToken = async function (token) {
     const user = await User.findByPk(id, {
       include: [db.models.object]
     });
-    console.log('user', user)
+    // console.log('user.dataValues.objects', user.dataValues.objects)
     if (!user) {
       throw "nooo";
     }
