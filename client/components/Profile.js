664e243ada8c179ea58378bcf1fc5d83519dbb2e
Jasmine Cheung <evilgummii@gmail.com>
2022-10-14 09:57:47 -0400

    added scroll to top button

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,9 +1,6 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
-/**
- * COMPONENT
- */
 const Profile = (props) => {
   const username = useSelector((state) => state.auth.username);
   const favorites = useSelector((state) => state.auth.objects);
