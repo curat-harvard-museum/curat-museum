55cf694715f467380f79280b4d5bf704509154a7
Jordy <jordynavalee@gmail.com>
2022-10-17 21:02:41 -0500

    WIP: single page styling

--- client/components/SingleObjectView.js
+++ client/components/SingleObjectView.js
@@ -13,13 +13,13 @@ function SingleObjectView() {
     );
   });
 
-  console.log(data?.data.images.map((image) => image.idsid));
+  console.log(data?.data);
 
   return (
     <>
       <div>
         {
-          <div>
+          <Box display="flex" flexWrap="wrap">
             <Image
               className="single-image"
               src={`${data?.data.primaryimageurl}`}
@@ -34,56 +34,87 @@ function SingleObjectView() {
                 bg={`${color.color}`}
               ></Circle>
             ))}
-            <Text>Title</Text>
+            <Text as="b" color="gray.300" fontSize="1.25rem">
+              Title
+            </Text>
             <Text>{data?.data.titles ? data?.data.titles[0].title : null}</Text>
             <Text>Artist</Text>
             <Text>{data?.data.people ? data?.data.people[0].name : null}</Text>
 
             {data?.data.images.map((image) => (
               <Image
+                key={image.displayorder}
                 w="15rem"
                 h="15rem"
                 src={`https://ids.lib.harvard.edu/ids/iiif/${image.idsid}/full/full/0/default.jpg`}
               ></Image>
             ))}
 
-            <Text>Century</Text>
+            <Text as="b" color="gray.300" fontSize="1.25rem">
+              Century
+            </Text>
             <Text>{data?.data.century ? data?.data.century : null}</Text>
 
-            <Text>Date of Completion</Text>
+            <Text as="b" color="gray.300" fontSize="1.25rem">
+              Date of Completion
+            </Text>
             <Text>{data?.data.dated ? data?.data.dated : null}</Text>
 
-            <Text>Culture</Text>
+            <Text as="b" color="gray.300" fontSize="1.25rem">
+              Culture
+            </Text>
             <Text>{data?.data.culture ? data?.data.culture : null}</Text>
 
-            <Text>Classification</Text>
+            <Text as="b" color="gray.300" fontSize="1.25rem">
+              Classification
+            </Text>
             <Text>
               {data?.data.classification ? data?.data.classification : null}
             </Text>
 
-            <Text>Medium/Technique</Text>
+            <Text as="b" color="gray.300" fontSize="1.25rem">
+              Medium/Technique
+            </Text>
             <Text>{data?.data.medium ? data?.data.medium : null}</Text>
             <Text>{data?.data.technique ? data?.data.technique : null}</Text>
 
             {data?.data.gallery ? (
-              <Text>
+              <Text as="b" color="gray.300" fontSize="1.25rem">
                 Location Floor
-                {data?.data.gallery.floor}
-                {data?.data.gallery.name}
+              </Text>
+            ) : null}
+
+            {data?.data.gallery ? (
+              <Text key={data?.data.gallery.id}>
+                {data?.data.gallery.floor},{data?.data.gallery.name},
                 {data?.data.gallery.number}
               </Text>
             ) : null}
 
-            <Text>Associated Exhibitions</Text>
+            <Text as="b" color="gray.300" fontSize="1.25rem">
+              Associated Exhibitions
+            </Text>
+            {data?.data.exhibitions
+              ? data?.data.exhibitions.map((exhibition) => (
+                  <Text>{exhibition.title}</Text>
+                ))
+              : null}
+
             <Text></Text>
 
-            <Text>Related Artworks</Text>
+            <Text as="b" color="gray.300" fontSize="1.25rem">
+              Related Artworks
+            </Text>
             <Text></Text>
 
-            <Text>{data?.data.title ? data?.data.title : null}</Text>
-            <Text>{data?.data.people ? data?.data.people[0].name : null}</Text>
+            <Text as="b" color="gray.300" fontSize="1.25rem">
+              Classification
+            </Text>
             <Text>{data?.data.classification}</Text>
-          </div>
+            <Text as="b" color="gray.300" fontSize="1.25rem">
+              Related Artworks
+            </Text>
+          </Box>
         }
       </div>
     </>
