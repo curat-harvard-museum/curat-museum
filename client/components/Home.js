920d8d51571b976b86d3198f3142ae29ae7c2918
Jasmine Cheung <evilgummii@gmail.com>
2022-10-20 18:15:52 -0400

    text overlay on bg

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,12 +1,49 @@
 import React from "react";
 import Exhibitions from "./Exhibitions";
 import BackToTopButton from "./BackToTopButton";
-import { Divider, Image, Show } from "@chakra-ui/react";
+import {
+  Divider,
+  Image,
+  Show,
+  Text,
+  Wrap,
+  WrapItem,
+  Center,
+} from "@chakra-ui/react";
 
-const Home = (props) => {
+const Home = () => {
   return (
     <div>
-      <Image src="/assets/images/curat-background.png" alt="" w="100%" />
+      <div className="bg-container">
+        <Image src="/assets/images/curat-background.png" alt="" w="100%" />
+        <Show breakpoint="(min-width: 1441px)">
+          <Text
+            className="bg-text"
+            as="b"
+            fontSize="3xl"
+            w="40%"
+            h="20%"
+            alignItems="center"
+          >
+            Curat your next museum trip with us and discover artwork more
+            deeply.
+          </Text>
+        </Show>
+
+        <Show breakpoint="(max-width: 1440px)">
+          <Text
+            className="bg-text"
+            as="b"
+            fontSize="sm"
+            w="40%"
+            h="20%"
+            alignItems="center"
+          >
+            Curat your next museum trip with us and discover artwork more
+            deeply.
+          </Text>
+        </Show>
+      </div>
       <br></br>
       <Divider />
       <br></br>
