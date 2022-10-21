a7a09de52ab8c96567b7bbfdeb3ca707353fb77a
Jasmine Cheung <evilgummii@gmail.com>
2022-10-21 00:44:33 -0400

    fixed home and exhibition mobile css

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,44 +1,21 @@
 import React from "react";
 import Exhibitions from "./Exhibitions";
 import BackToTopButton from "./BackToTopButton";
-import {
-  Divider,
-  Image,
-  Show,
-  Text,
-  Wrap,
-  WrapItem,
-  Center,
-} from "@chakra-ui/react";
+import { Divider, Image, Show, Text } from "@chakra-ui/react";
 
 const Home = () => {
   return (
     <div>
       <div className="bg-container">
         <Image src="/assets/images/curat-background.png" alt="" w="100%" />
-        <Show breakpoint="(min-width: 1441px)">
-          <Text
-            className="bg-text"
-            as="b"
-            fontSize="3xl"
-            w="40%"
-            h="20%"
-            alignItems="center"
-          >
+        <div className="bg-text">
+          <Text as="b" fontSize="2xl">
             Curat your next museum trip with us and discover artwork more
             deeply.
           </Text>
-        </Show>
-
-        <Show breakpoint="(max-width: 1440px)">
-          <Text
-            className="bg-text"
-            as="b"
-            fontSize="sm"
-            w="40%"
-            h="20%"
-            alignItems="center"
-          >
+        </div>
+        <Show breakpoint="(max-width: 695px)">
+          <Text as="b" fontSize="md">
             Curat your next museum trip with us and discover artwork more
             deeply.
           </Text>
